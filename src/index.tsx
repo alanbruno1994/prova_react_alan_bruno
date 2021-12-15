01de101fece14ea00f8157560a9571cbee26a356
alanbruno1994 <bruno47rios@outlook.com>
2021-12-15 19:07:00 -0300

    refactor: removed index.css and replaced with GlobalStyled

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from '@src/App';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './globalStyled';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
+    <GlobalStyle/>
   </BrowserRouter>,
   document.getElementById('root')
 );
