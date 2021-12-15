cf6e3656c122c1f354e24c247193f5fa90bd2119
alanbruno1994 <bruno47rios@outlook.com>
2021-12-15 20:02:32 -0300

    feat: insert RegionForm in RegisterUser

--- src/pages/RegisterUser.tsx
+++ src/pages/RegisterUser.tsx
@@ -1,10 +1,23 @@
 import Brand from "@components/Brand"
 import TwoViews from "@components/TwoViews"
+import RegionForm from "@components/RegionForm"
+import { ButtonLogin } from "@components/RegionFormStyled"
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs"
+import { NavLink } from "react-router-dom"
 
 const RegisterUser=()=>{
     return  <TwoViews>
-    <div><Brand/></div>
-    <div></div>        
+            <div><Brand/></div>
+            <div><RegionForm title="Reset Password" linkFooter={<NavLink to="/"> <BsArrowLeft size={24}/> Back</NavLink>}>
+                        <input type="text" placeholder="Name"></input>
+                        <div className="spacing"/>      
+                        <input type="email" placeholder="Email"></input>
+                        <div className="spacing"/>      
+                        <input type="password" placeholder="Password"></input>
+                        <div className="spacing"/>                       
+                        <ButtonLogin>Register <BsArrowRight size={24}/></ButtonLogin>                
+                </RegionForm>    
+            </div>        
     </TwoViews>
 }
 
