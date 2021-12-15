733ce4542bb25b411176218a0dd1dd256a9bde76
alanbruno1994 <bruno47rios@outlook.com>
2021-12-15 11:21:39 -0300

    feat: add select routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import Home from '@pages/Home';
+import Login from '@pages/Login';
+import NewBet from '@pages/NewBet';
+import RegisterUser from '@pages/RegisterUser';
+import ResetPassoword from '@pages/ResetPassword';
+import { Route, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Switch>
+      <Route path="/" exact >
+        <Login/>
+      </Route>
+      <Route path="/home">
+        <Home/>
+      </Route>
+      <Route path="/new-bet">
+        <NewBet/>
+      </Route>
+      <Route path="/reset-password">
+        <ResetPassoword/>
+      </Route>
+      <Route path="/register">
+        <RegisterUser/>
+      </Route>
+
+    </Switch>
   );
 }
 
