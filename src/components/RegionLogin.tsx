12544ffd0dc60166d72eceddf267f4162c3ce316
alanbruno1994 <bruno47rios@outlook.com>
2021-12-15 15:21:03 -0300

    feat: add Card and Form in RegionLogin

--- src/components/RegionLogin.tsx
+++ src/components/RegionLogin.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"
 import styled from "styled-components"
 import Card from "./UI/Card"
 
@@ -17,11 +18,57 @@ const DIVFather=styled.div`
 
 `
 
+const Form=styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    & input{
+        font-size: 17px;
+        font: italic normal bold 17px Helvetica Neue;
+        color: #9D9D9D;
+        padding: 30px;
+        border: 0px;
+    }
+
+    & input:hover, & input:active
+    {
+        border:0px
+    }
+
+    &>div
+    {
+        height: 2px;
+        background:#EBEBEB;
+    }
+
+    &>a{
+        color: #C1C1C1;
+        font-style: normal;
+        text-decoration:none;
+        display: flex;
+        justify-content: end;
+        margin-right: 30px;
+        margin-top: 20px;
+        span{
+                    
+        }
+    }
+`
+
 const RegionLogin: React.FC=(props)=>{
 
     return <DIVFather>
                 <span>Authentication</span>
-                <Card></Card>
+                <Card>
+                    <Form>
+                        <input type="text" placeholder="Email"></input>
+                        <div className="spacing"/>
+                        <input type="password" placeholder="Password"></input>
+                        <div className="spacing"/>
+                        <NavLink to="/reset-password"><span>I me forget password</span></NavLink>
+                    </Form>
+                </Card>
             </DIVFather>
 }
 
