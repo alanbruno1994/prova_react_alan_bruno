cad0cd36ddb419f156b1373a9c07abf3c1d5d716
alanbruno1994 <bruno47rios@outlook.com>
2021-12-15 14:25:33 -0300

    feat: add component Brand

---
+++ src/components/Brand.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components"
+
+const DIVBrand= styled.div`
+    &.brand{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        width: 100%;    
+    }    
+`
+
+const DIVBrandIntern= styled.div`
+    &.organization{
+        display: flex;
+        flex-direction: column;
+        font-family: Helvetica, Arial, sans-serif;
+        font-style: italic;
+        font-size: 65px;
+        font-weight: bolder;
+        color:#707070;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+    }
+`
+
+
+const FOR= styled.div`
+    background-color: #B5C401;   
+    width: 144px;
+    height: 39px;  
+    border-radius: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+
+   & span{
+        font-size: 22px;
+        color: white;
+        text-align: center;
+        font-style: italic;
+        font-weight: bold;       
+    }
+    
+`
+
+const Brand=()=>{
+
+    return <DIVBrand className="brand">
+                <DIVBrandIntern className="organization">
+                    <span>The </span>
+                    <span>Geatest</span>
+                    <span>App</span>
+                    <FOR><span>for</span></FOR> 
+                    <span style={{textTransform:'uppercase'}}> Lottery</span>   
+                </DIVBrandIntern>             
+        </DIVBrand>
+}
+
+export default Brand
