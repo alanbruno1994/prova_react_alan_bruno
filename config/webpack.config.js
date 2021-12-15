49fa6b8b33082998afef73c701941762b1e251b2
alanbruno1994 <bruno47rios@outlook.com>
2021-12-15 10:38:08 -0300

    refactor: module aliases

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -308,7 +308,7 @@ module.exports = function (webpackEnv) {
         .filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
         '@components':path.resolve(__dirname,'../src/components/'),
-        '@views':path.resolve(__dirname,'../src/views/'),
+        '@pages':path.resolve(__dirname,'../src/pags/'),
         '@src':path.resolve(__dirname,'../src/'),
          // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
