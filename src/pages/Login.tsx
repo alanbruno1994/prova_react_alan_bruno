f716b3ccc8dc68a5313c4c306747addec97c01da
alanbruno1994 <bruno47rios@outlook.com>
2021-12-15 19:54:19 -0300

    feat: in Login insert content RegionForm

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,9 @@
 import Brand from "@components/Brand"
-import RegionLogin from "@components/RegionLogin"
+import RegionForm from "@components/RegionForm"
+import { ButtonLogin } from "@components/RegionFormStyled"
 import TwoViews from "@components/TwoViews"
+import { BsArrowRight } from "react-icons/bs"
+import { NavLink } from "react-router-dom"
 
 
 const Login=()=>{
@@ -8,7 +11,15 @@ const Login=()=>{
     return <>
     <TwoViews>
             <div><Brand/></div>
-            <div><RegionLogin></RegionLogin></div>        
+            <div><RegionForm linkFooter={<NavLink to="/register">Sign Up <BsArrowRight size={24}/></NavLink>}>
+                        <input type="text" placeholder="Email"></input>
+                        <div className="spacing"/>
+                        <input type="password" placeholder="Password"></input>
+                        <div className="spacing"/>
+                        <NavLink to="/reset-password"><span>I forget my password</span></NavLink>
+                        <ButtonLogin>Log in <BsArrowRight size={24}/></ButtonLogin>                
+                </RegionForm>
+            </div>        
     </TwoViews>
     </>
 }
