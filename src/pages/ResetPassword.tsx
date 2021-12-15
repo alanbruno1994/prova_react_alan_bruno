88d38c7dce597e2c3efb7cbdd9c96f747c399aac
alanbruno1994 <bruno47rios@outlook.com>
2021-12-15 20:11:02 -0300

    feat: add ResetPassword

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,10 +1,19 @@
 import Brand from "@components/Brand"
 import TwoViews from "@components/TwoViews"
+import RegionForm from "@components/RegionForm"
+import { NavLink } from "react-router-dom"
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs"
+import { ButtonLogin } from "@components/RegionFormStyled"
 
 const ResetPassoword=()=>{    
     return  <TwoViews>
             <div><Brand/></div>
-            <div></div>        
+            <div><RegionForm title="Reset Password" linkFooter={<NavLink to="/"> <BsArrowLeft size={24}/> Back</NavLink>}>                            
+                        <input type="email" placeholder="Email"></input>
+                        <div className="spacing"/>                                          
+                        <ButtonLogin>Send Link <BsArrowRight size={24}/></ButtonLogin>                
+                </RegionForm>    
+            </div>        
             </TwoViews>
 }
 export default ResetPassoword
