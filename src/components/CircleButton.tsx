1a22167ef50301004f12138871508b99870aff45
alanbruno1994 <bruno47rios@outlook.com>
2021-12-16 15:30:02 -0300

    feat: add component CircleButton

---
+++ src/components/CircleButton.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+const Button=styled.button`
+    width: 65px;
+    height: 65px;
+    border-radius: 50%;
+    border:0px;
+    background: #ADC0C4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 20px;
+    font-weight: bold;
+`
+
+const CicleButton:React.FC<{number:string}>=(props)=>{
+    return <Button><span>{props.number}</span></Button>    
+}
+
+
+export default CicleButton
