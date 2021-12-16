81627f6a8611b0a72b916b45937d86e0eab2fae8
alanbruno1994 <bruno47rios@outlook.com>
2021-12-16 10:26:35 -0300

    refactor: altered TwoViews

--- src/components/TwoViews.tsx
+++ src/components/TwoViews.tsx
@@ -2,12 +2,14 @@ import styled from 'styled-components'
 
 const Div=styled.div<{isHeader:boolean}>`
     display:flex;
-    height: ${ (props)=> (!props.isHeader ? 'calc(100vh - 70px);' : 'calc(100vh - 142px);' )}
+    width: 85%;
+    margin: 0 auto;
+    height: ${ (props)=> (!props.isHeader ? 'calc(100vh - 70px)' : 'calc(100vh - 142px)' )};   
+   
     &>div{
         width:50%;        
     }
-    width: 85%;
-    margin: 0 auto;
+    
 `
 const TwoViews:React.FC<{header:boolean}>=(props)=>{
    
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,10 @@ const Home=()=>
             <div className="TGLHome">TGL<div/></div>
             <div className="TwoElements"><span><span>Account</span></span><button onClick={exitHandler}>Log out &nbsp; <BsArrowRight size={24}/></button></div>
         </Header>
-        <TwoViews header={true}></TwoViews>
+        <TwoViews header={true}>
+            <div>Recent Games</div>
+            <div>New Bet</div>
+        </TwoViews>
         <Footer content="Copyright 2021 Luby Software"/>    
     </>
 }
