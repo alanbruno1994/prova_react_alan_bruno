eb3f087a364d0dfff6d3949fd76b8903999875db
alanbruno1994 <bruno47rios@outlook.com>
2021-12-16 11:32:28 -0300

    feat: add ButtonGame

---
+++ src/components/ButtonGame.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const Button=styled.button<{colorText:string,backGround:string}>`
+    background: ${props=>props.backGround};
+    color:${props=>props.colorText};
+    width: 113px;
+    height: 34px;
+    border-radius: 100px;
+
+`
+
+const ButtonGame:React.FC<{text:string,backGround:string,colorText:string}>=(props)=>
+{
+
+    return <Button backGround={props.backGround} colorText={props.colorText}>{props.text}</Button>
+
+}
+
+export default ButtonGame
