4a333782d32c5acd4ae42f653eabe63030715280
alanbruno1994 <bruno47rios@outlook.com>
2021-12-17 10:41:42 -0300

    feat:  add operation of login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,27 +3,44 @@ import Footer from "@components/footer"
 import RegionForm from "@components/RegionForm"
 import { ButtonLogin } from "@components/RegionFormStyled"
 import TwoViews from "@components/TwoViews"
-import React from "react"
+import React, { useState } from "react"
 import { BsArrowRight } from "react-icons/bs"
 import { NavLink, useHistory } from "react-router-dom"
+import {useDispatch} from 'react-redux'
+import axios from "axios"
+import { urlBase,headerBase } from "@src/constants/api_constants"
+import { loginActions } from "@src/store/login"
 
 
 const Login=()=>{
     const navigate=useHistory()
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('')
+    const dispatch=useDispatch();
 
-    const loginHandler=(event:React.FormEvent)=>
+    const loginHandler= async (event:React.FormEvent)=>
     {
-        event.preventDefault()
-        navigate.push("/home")        
+        event.preventDefault()      
+        const response = await axios.post(urlBase+'login',{email,password},{headers:headerBase})
+        const data = await response.data
+        const token= data.token.token;
+        const expireAtToken=data.token.expires_at;        
+        const user_id=data.user.id;
+        const name=data.user.name; 
+        const logged=true;
+        dispatch(loginActions.setLogin({token,expireAtToken,user_id,name,logged,email:data.user.email}))
+        navigate.replace("/home")
     }
+
+    
 
     return <>
             <TwoViews header={false}>
                     <div><Brand/></div>
                     <div><RegionForm action={loginHandler} title="Authentication" linkFooter={<NavLink to="/register">Sign Up <BsArrowRight size={24}/></NavLink>}>
-                                <input type="text" placeholder="Email"></input>
+                                <input type="text" placeholder="Email" onChange={event=>setEmail(event.target.value)}></input>
                                 <div className="spacing"/>
-                                <input type="password" placeholder="Password"></input>
+                                <input type="password" placeholder="Password" onChange={event=>setPassword(event.target.value)}></input>
                                 <div className="spacing"/>
                                 <NavLink to="/reset-password"><span>I forget my password</span></NavLink>
                                 <ButtonLogin>Log in <BsArrowRight size={24}/></ButtonLogin>                
