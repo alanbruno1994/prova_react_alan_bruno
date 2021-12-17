e21424b7222c6829de501723eec3d219adbea3be
alanbruno1994 <bruno47rios@outlook.com>
2021-12-17 11:38:39 -0300

    chore: add alias module hooks

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -312,6 +312,7 @@ module.exports = function (webpackEnv) {
         '@src':path.resolve(__dirname,'../src/'),
         '@store':path.resolve(__dirname,'../src/store'),
         '@constants':path.resolve(__dirname,'../src/constants'),
+        '@hooks':path.resolve(__dirname,'../src/hooks'),
          // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
