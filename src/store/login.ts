0a36e5150a21b8402eb247c66940ddc401f860a7
alanbruno1994 <bruno47rios@outlook.com>
2021-12-17 09:24:55 -0300

    feat: add elements to redux login

--- src/store/login.ts
+++ src/store/login.ts
@@ -1,21 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+type LoginRedux={
+  token:string
+  expireAtToken:null| Date
+  name:string
+  user_id:number
+  logged:boolean
+  email:string
+}
 
-const initialName = {
+
+const initialName:LoginRedux = {
     token: '',
+    expireAtToken:null,
+    name: '',
+    user_id:0,
+    logged:false,
+    email:'',    
   };
   
   const loginSlice = createSlice({
     name: 'login',
     initialState: initialName,
     reducers: {//Aqui nao que action precisao tenha o type
-      setToken(state,action) {
-        state.token=action.payload
+      setLogin(state,action) {      
+       state=action.payload
+      },
+      setLogged(state,action)
+      {
+        state.logged=action.payload
       }
     },
   });
   
 
-  export const nameActions = loginSlice.actions;
+  export const  loginActions = loginSlice.actions;
   
   export default loginSlice.reducer;
