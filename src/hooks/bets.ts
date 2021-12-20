99d0cbd33ded1e44c964b6e051e2cc2c3e494204
alanbruno1994 <bruno47rios@outlook.com>
2021-12-20 15:24:16 -0300

    feat: add useBet failure treatment

--- src/hooks/bets.ts
+++ src/hooks/bets.ts
@@ -1,22 +1,27 @@
+import mensagesFailure from "@src/common/messages_failure";
 import { headerBase, urlBase } from "@src/constants/api_constants";
 import Bet from "@src/types/bet.type";
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
-export default function useBets() {
+export default function useBets(openFailure: any) {
   const [bets, setBets] = useState<Bet[]>([]);
   const login = useSelector((state: any) => state.login);
 
   const getBets = useCallback(
     async (params: string = "") => {
-      const response = await axios.get(urlBase + "bet/all-bets" + params, {
-        headers: { ...headerBase, Authorization: "Bearer " + login.token },
-      });
-      const data = await response.data;
-      setBets(data);
+      try {
+        const response = await axios.get(urlBase + "bet/all-bets" + params, {
+          headers: { ...headerBase, Authorization: "Bearer " + login.token },
+        });
+        const data = await response.data;
+        setBets(data);
+      } catch (failure: any) {
+        mensagesFailure(failure, openFailure);
+      }
     },
-    [login.token]
+    [login.token, openFailure]
   );
 
   function formatPriceCreateAt(bet: Bet): string {
