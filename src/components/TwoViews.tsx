30a32df20516ed20ec4fe82c39babcb1ecdef35c
alanbruno1994 <bruno47rios@outlook.com>
2021-12-20 10:12:42 -0300

    feat: modify style of TwoViews

--- src/components/TwoViews.tsx
+++ src/components/TwoViews.tsx
@@ -1,136 +1,128 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Div=styled.div<{isHeader:boolean,home?:boolean}>`
-    display:flex;
-    width: 85%;
-    margin: 0 auto;
-    padding-top: ${props=> props.home ? '70px' : '0px'};
-    height: ${ (props)=> (!props.isHeader ? 'calc(100vh - 70px)' : 'calc(100vh - 142px)' )};    
-    
-    &>div{
-        width:50%;        
-    }
-    
-    .leftDiv{
-        width: 80%;
-        .header-top-recent-games{
-            display: flex;
+const Div = styled.div<{ isHeader: boolean; home?: boolean }>`
+  display: flex;
+  flex-wrap: wrap;
+  width: 85%;
+  margin: 0 auto;
+  padding-top: ${(props) => (props.home ? "70px" : "0px")};
+  min-height: ${(props) =>
+    !props.isHeader ? "calc(100vh - 70px)" : "calc(100vh - 142px)"};
+  max-height: auto;
 
-            .title-recent{
-                  font-size: 24px;
-                  text-transform: uppercase;
-                  color: #707070;
-                  font-weight: bold;
-                  font-style: italic;
-                  margin-right: 60px;
-            }
-            
-            .filter-div{
-             display:flex;
-             justify-content: center;
-             align-items: center;
-             
+  & > div {
+    height: 100%;
+    width: 50%;
+  }
 
-                .filter{
-                        font-size: 17px;
-                        color: #868686;
-                        font-style: italic;
-                        font-weight: 100;
-                }
-             
-                button
-                {
-                    margin:0px 15px;
-                }
+  .leftDiv {
+    width: 80%;
+    .header-top-recent-games {
+      display: flex;
 
-            }
-
-            
-         }  
-         
-         .region-game{
-                display: flex;
-                flex-direction: column;
-
-                .titles
-                {
-                    margin-top: 60px;
-                    margin-bottom: 33px;
-                }
-
-                .title-bold,.title
-                {
-                    text-transform: uppercase;
-                    color:#707070;
-                    font-size: 24px;
-                    font-style: italic;
-                    font-weight: 100;
-                }
-
-                .title-bold{
-                    font-weight: bold;
-                }
-
-                .subtitle
-                {
-                    color:#707070;
-                    font-size: 17px;
-                    font-style: italic;
-                }
-
-                .game-change{
-                    display: flex;
-                    margin-top: 30px;
-                    margin-bottom: 20px;
-                    
-                    button{
-                        margin-right: 25px;
-                    }
-                }
-
-                .number-chooses{
-                    display: flex;
-                    margin-top: 20px;
-                    flex-wrap: wrap;
-                }
-
-         }
-
-
-         
-
-         
+      .title-recent {
+        font-size: 24px;
+        text-transform: uppercase;
+        color: #707070;
+        font-weight: bold;
+        font-style: italic;
+        margin-right: 60px;
+      }
     }
 
-    .rightDiv{
-        width: 20%;
+    .region-game {
+      display: flex;
+      flex-direction: column;
 
+      .titles {
+        margin-top: 60px;
+        margin-bottom: 13px;
+      }
 
+      .title-bold,
+      .title {
+        text-transform: uppercase;
+        color: #707070;
+        font-size: 24px;
+        font-style: italic;
+        font-weight: 100;
+      }
+
+      .title-bold {
+        font-weight: bold;
+      }
+
+      .subtitle {
+        color: #707070;
+        font-size: 17px;
+        font-style: italic;
+        margin-bottom: 15px;
+        margin-top: 15px;
+      }
+
+      .game-change {
+        display: flex;
+        margin-top: 30px;
+        margin-bottom: 20px;
+
+        button {
+          margin-right: 25px;
+        }
+      }
     }
-    .DivContentNewBet{
+  }
+
+  .rightDiv {
+    width: 20%;
+  }
+  .DivContentNewBet {
     display: flex;
     justify-content: flex-end;
 
-    .NewBet{
-        font-size: 24px;
-        text-decoration:none;
-        color: #B5C401;
-        font-weight: bold;
-        font-style: italic;
-        text-align:right;
+    .NewBet {
+      font-size: 24px;
+      text-decoration: none;
+      color: #b5c401;
+      font-weight: bold;
+      font-style: italic;
+      text-align: right;
+    }
+  }
+
+  @media (max-width: 1330px) {
+    .leftDiv,
+    .rightDiv {
+      width: 100%;
     }
 
-   
-}
+    .DivContentNewBet {
+      justify-content: flex-start;
 
+      .NewBet {
+        text-align: left;
+      }
+    }
+  }
 
+  @media (max-width: 900px) {
+    .header-top-recent-games {
+      flex-direction: column;
+    }
+  }
 
-`
-const TwoViews:React.FC<{header:boolean,home?:boolean}>=(props)=>{
-   
-    return <Div isHeader={props.header} home={props.home}>
-            {props.children}
-           </Div>
-}
+  @media (max-width: 360px) {
+    width: 98%;
+    .title-recent {
+      font-size: 18px;
+    }
+  }
+`;
+const TwoViews: React.FC<{ header: boolean; home?: boolean }> = (props) => {
+  return (
+    <Div isHeader={props.header} home={props.home}>
+      {props.children}
+    </Div>
+  );
+};
 
-export default TwoViews
+export default TwoViews;
