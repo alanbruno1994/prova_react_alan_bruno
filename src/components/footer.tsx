54897296940378a26d62f001729a7f7d15c9a42f
alanbruno1994 <bruno47rios@outlook.com>
2021-12-20 10:17:23 -0300

    refactor: modify code

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,34 +1,37 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const FooterBody=styled.div`
+const FooterBody = styled.div`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+
+  & > div {
     width: 100%;
-    height: 70px;
+    height: 2px;
+    background-color: #ebebeb;
+  }
+
+  span {
+    height: 100%;
     display: flex;
-    flex-direction: column;
-    text-align:center;
-   
+    justify-content: center;
+    align-items: center;
+    color: #707070;
+    font-size: 15px;
+  }
+`;
 
-    &>div
-    {
-        width: 100%;
-        height: 2px;
-        background-color: #EBEBEB;
-    }
+const Footer: React.FC<{ content: string }> = (props) => {
+  return (
+    <FooterBody>
+      <div />
+      <span>
+        <span>{props.content}</span>
+      </span>
+    </FooterBody>
+  );
+};
 
-    span{
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color:#707070;
-        font-size: 15px;
-        
-    }
-`
-
-const Footer:React.FC<{content:string}>=(props)=>{
-
-    return <FooterBody><div/><span><span>{props.content}</span></span></FooterBody>
-}
-
-export default Footer
+export default Footer;
