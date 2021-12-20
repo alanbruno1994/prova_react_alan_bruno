bc5e6ec1bb1136995467d39dea130cc0265e02d4
alanbruno1994 <bruno47rios@outlook.com>
2021-12-20 09:38:40 -0300

    feat: add styled and props in RectangleButton

--- src/components/Button/RectangleButton.tsx
+++ src/components/Button/RectangleButton.tsx
@@ -1,31 +1,43 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const Button=styled.button`
-    background: #F7F7F7;
-    font-size: 16px;
-    color: #27C383;    
-    box-sizing: 1px;
-    border-color: #27C383;
-    border-radius: 10px;
-    height: 52px;
-    
-    .content
-    {
-        margin: 0px 20px;
+const Button = styled.button<{
+  background?: string;
+  colorText?: string;
+  marginRight?: string;
+}>`
+  background: ${(props) => (props.background ? props.background : "#F7F7F7")};
+  font-size: 16px;
+  color: ${(props) => (props.colorText ? props.colorText : "#27C383")};
+  box-sizing: 1px;
+  border-color: #27c383;
+  border-radius: 10px;
+  height: 52px;
+  margin-right: ${(props) => (props.marginRight ? props.marginRight : "0px")};
+  .content {
+    margin: 0px 20px;
 
-        .separate{
-            margin-left: 12px;
-        }
+    .separate {
+      margin-left: 12px;
     }
+  }
+`;
 
-        
-   
+const RectangleButton: React.FC<{
+  background?: string;
+  colorText?: string;
+  marginRight?: string;
+  action?: (value: any) => void;
+}> = (props) => {
+  return (
+    <Button
+      onClick={props.action}
+      marginRight={props.marginRight}
+      background={props.background}
+      colorText={props.colorText}
+    >
+      <span className="content">{props.children}</span>
+    </Button>
+  );
+};
 
-`
-
-const RectangleButton:React.FC=(props)=>
-{
-    return <Button><span className="content">{props.children}</span></Button>
-}
-
-export default RectangleButton
+export default RectangleButton;
