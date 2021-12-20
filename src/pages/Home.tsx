7e7769443909e3b7e54878c71bc39aad1553fe15
alanbruno1994 <bruno47rios@outlook.com>
2021-12-20 15:25:05 -0300

    refactor: modify Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,40 +3,20 @@ import MyGamesRecents from "@components/MyGamesRecents";
 import TwoViews from "@components/TwoViews";
 import { BsArrowRight } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
-import Game from "@src/types/game.type";
-import axios from "axios";
-import { headerBase, urlBase } from "@src/constants/api_constants";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Header from "@components/Header/Header";
 import FilterRegion from "@components/FilterRegion";
-import mensagesFailure from "@src/common/messages_failure";
 import ErrorPortal from "@src/portals/ErrorPortal";
-import useFailure from "@src/hooks/failure";
 import Error from "@components/UI/Error";
 import useBets from "@src/hooks/bets";
+import useGames from "@src/hooks/games";
+import useFailure from "@src/hooks/failure";
 
 const Home = () => {
-  const [games, setGames] = useState<Game[]>([]);
   const [chooseGame, setChooseGame] = useState({ id: 0, index: 0 });
   const { failure, openFailure, closeFailure } = useFailure();
-  const { bets, getBets, formatPriceCreateAt } = useBets();
-
-  const serveData = async () => {
-    try {
-      const response = await axios.get(urlBase + "cart_games", {
-        headers: headerBase,
-      });
-      const data = await response.data;
-      setGames(data.types);
-      getBets();
-    } catch (failure: any) {
-      mensagesFailure(failure, openFailure);
-    }
-  };
-
-  useEffect(() => {
-    serveData();
-  }, []);
+  const { games } = useGames(openFailure);
+  const { bets, getBets, formatPriceCreateAt } = useBets(openFailure);
 
   const handlerChooseGame = (value: { id: number; index: number }) => {
     setChooseGame(value);
