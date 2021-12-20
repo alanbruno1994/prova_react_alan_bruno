6c24c78f41352f013bd1a781dcfb833a1f641b77
alanbruno1994 <bruno47rios@outlook.com>
2021-12-20 11:12:38 -0300

    feat: add E_UNAUTHORIZED_ACCESS in messages_failure

--- src/common/messages_failure.ts
+++ src/common/messages_failure.ts
@@ -10,7 +10,15 @@ export default function mensagesFailure(
       if (failure.response && failure.response.status === 400) {
         openFailure(failure.response.data.error.message);
       } else if (failure.response && failure.response.status === 401) {
-        openFailure(failure.response.data.message);
+        if (failure.response.data.message)
+          openFailure(failure.response.data.message);
+        else if (
+          failure.response.data.errors[0].message ===
+          "E_UNAUTHORIZED_ACCESS: Unauthorized access"
+        )
+          openFailure(
+            "You are not authorizing to do this, check if you are correctly logged in to perform this operation."
+          );
       } else {
         openFailure(failure.message);
       }
