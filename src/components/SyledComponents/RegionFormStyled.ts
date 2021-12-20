d102fa4b8d45bd188c1095654d8a8aef6298c981
alanbruno1994 <bruno47rios@outlook.com>
2021-12-20 10:23:44 -0300

    feat: modify logic in RegionFormStyled

--- src/components/SyledComponents/RegionFormStyled.ts
+++ src/components/SyledComponents/RegionFormStyled.ts
@@ -1,78 +1,73 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-const DIVFather=styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
+const DIVFather = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: calc(100vh - 70px);
 
-    &>span, &>a
-    {
-       
-        font-size: 35px;
-        font-style: italic;
-        font-weight: bold;
-        color: #707070;
-    }
-
-    &>a{
-        text-decoration: none;
-    }
-
-
-`
-
-const Form=styled.form`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-
-    & input{
-        font-size: 17px;
-        font: italic normal bold 17px Helvetica Neue;
-        color: #9D9D9D;
-        padding: 30px;
-        border: 0px;
-    }
-
-    & input:hover, & input:active
-    {
-        border:0px
-    }
-
-    &>div
-    {
-        height: 2px;
-        background:#EBEBEB;
-    }
-
-    &>a{
-        color: #C1C1C1;
-        font-style: normal;
-        text-decoration:none;
-        display: flex;
-        justify-content: end;
-        margin-right: 30px;
-        margin-top: 20px;
-        font-style: italic;
-        font-weight: 400;
-    }
-`
-
-const ButtonLogin=styled.button`
-    color: #B5C401;
-    background-color: white;
-    border:0;
+  & > span,
+  & > a {
     font-size: 35px;
+    font-style: italic;
     font-weight: bold;
-    font-family: Helvetica;
-    font-style: italic;   
-    cursor: pointer;
-    display: block;
-    margin: 40px auto;
-    
-`
+    color: #707070;
+  }
 
-export { ButtonLogin,DIVFather, Form}
+  & > a {
+    text-decoration: none;
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  & input {
+    font-size: 17px;
+    font: italic normal bold 17px Helvetica Neue;
+    color: #9d9d9d;
+    padding: 30px;
+    border: 0px;
+  }
+
+  & input:hover,
+  & input:active {
+    border: 0px;
+  }
+
+  & > div {
+    height: 2px;
+    background: #ebebeb;
+  }
+
+  & > a {
+    color: #c1c1c1;
+    font-style: normal;
+    text-decoration: none;
+    display: flex;
+    justify-content: end;
+    margin-right: 30px;
+    margin-top: 20px;
+    font-style: italic;
+    font-weight: 400;
+  }
+`;
+
+const ButtonLogin = styled.button`
+  color: #b5c401;
+  background-color: white;
+  border: 0;
+  font-size: 35px;
+  font-weight: bold;
+  font-family: Helvetica;
+  font-style: italic;
+  cursor: pointer;
+  display: block;
+  margin: 40px auto;
+`;
+
+export { ButtonLogin, DIVFather, Form };
