4c6948d0e469ecee72e9cbbc4b8bec4a846b2788
alanbruno1994 <bruno47rios@outlook.com>
2021-12-20 09:19:22 -0300

    refactor: moved buttons for buttons folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,80 +1,118 @@
-import ButtonGame from "@components/ButtonGame";
+import ButtonGame from "@components/Button/ButtonGame";
 import Footer from "@components/footer";
 import Header from "@components/Header";
 import MyGamesRecents from "@components/MyGamesRecents";
 import TwoViews from "@components/TwoViews";
 import { BsArrowRight } from "react-icons/bs";
-import { NavLink,  } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import useLogout from "@src/hooks/logout";
 import Game from "@src/types/game.type";
 import axios from "axios";
 import { headerBase, urlBase } from "@src/constants/api_constants";
 import { useEffect, useState } from "react";
 
+const Home = () => {
+  const logoutHandler = useLogout();
+  const [games, setGames] = useState<Game[]>([]);
+  const [chooseGame, setChooseGame] = useState(0);
 
-const Home=()=>
-{
-   
-    const logoutHandler = useLogout()
-    const [games,setGames]= useState<Game[]>([])
-    const [chooseGame,setChooseGame]=useState(0)
-  
+  const serveData = async () => {
+    const response = await axios.get(urlBase + "cart_games", {
+      headers: headerBase,
+    });
+    const data = await response.data;
+    setGames(data.types);
+    setChooseGame(data.types[0].id);
+  };
 
+  useEffect(() => {
+    serveData();
+  }, []);
 
-    const serveData= async ()=>
-    {
-        const response = await axios.get(urlBase+'cart_games',{headers:headerBase}) 
-        const data= await response.data
-        setGames(data.types) 
-        setChooseGame(data.types[0].id)     
-    }
+  const chooseGameHandler = (choose: number, index: number) => {
+    setChooseGame(choose);
+  };
 
-    useEffect(()=>{
-        serveData()
-       },[])
-
-    const chooseGameHandler=(choose:number,index:number)=>
-    {
-        setChooseGame(choose)       
-    }
-  
-    return <>
-        <Header>
-            <div className="TGLHome">TGL<div/></div>
-            <div className="TwoElements"><span><span>Account</span></span><button onClick={logoutHandler}>Log out &nbsp; <BsArrowRight size={24}/></button></div>
-        </Header>
-        <TwoViews header={true} home={true}>
-            <div className="leftDiv">
-                <div className="header-top-recent-games">
-                    <span className="title-recent">Recent Games</span>
-                    <div className="filter-div">
-                        <span className="filter">Filters</span>
-                        {games.map((game:Game,index)=>{                                  
-                                    if(game.id===chooseGame)
-                                    {
-                                        return <ButtonGame index={index} id={game.id} chooseHandler={chooseGameHandler} key={game.id} text={game.type} backGround={game.color} colorText="white"/>
-                                    }                                  
-                                   return  <ButtonGame  index={index} id={game.id} chooseHandler={chooseGameHandler} key={game.id} text={game.type} backGround="white" colorText={game.color}/>
-                                })}   
-                    </div>
-                </div>  
-                <MyGamesRecents numbers="01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25"
-                priceDate="30/11/2020 - (R$ 2,50)"
-                typeGame="Lotofácil"
-                typeColor="#7F3992"/>   
-                <MyGamesRecents numbers="01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25"
-                priceDate="30/11/2020 - (R$ 2,50)"
-                typeGame="Lotofácil"
-                typeColor="#7F3992"/> 
-                <MyGamesRecents numbers="01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25"
-                priceDate="30/11/2020 - (R$ 2,50)"
-                typeGame="Lotofácil"
-                typeColor="#7F3992"/>                  
+  return (
+    <>
+      <Header>
+        <div className="TGLHome">
+          TGL
+          <div />
+        </div>
+        <div className="TwoElements">
+          <span>
+            <span>Account</span>
+          </span>
+          <button onClick={logoutHandler}>
+            Log out &nbsp; <BsArrowRight size={24} />
+          </button>
+        </div>
+      </Header>
+      <TwoViews header={true} home={true}>
+        <div className="leftDiv">
+          <div className="header-top-recent-games">
+            <span className="title-recent">Recent Games</span>
+            <div className="filter-div">
+              <span className="filter">Filters</span>
+              {games.map((game: Game, index) => {
+                if (game.id === chooseGame) {
+                  return (
+                    <ButtonGame
+                      index={index}
+                      id={game.id}
+                      chooseHandler={chooseGameHandler}
+                      key={game.id}
+                      text={game.type}
+                      backGround={game.color}
+                      colorText="white"
+                    />
+                  );
+                }
+                return (
+                  <ButtonGame
+                    index={index}
+                    id={game.id}
+                    chooseHandler={chooseGameHandler}
+                    key={game.id}
+                    text={game.type}
+                    backGround="white"
+                    colorText={game.color}
+                  />
+                );
+              })}
             </div>
-            <div className="rightDiv"><div className="DivContentNewBet"><NavLink to="/new-bet" className="NewBet">New Bet <BsArrowRight size={24}/></NavLink></div></div>
-        </TwoViews>
-        <Footer content="Copyright 2021 Luby Software"/>    
+          </div>
+          <MyGamesRecents
+            numbers="01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25"
+            priceDate="30/11/2020 - (R$ 2,50)"
+            typeGame="Lotofácil"
+            typeColor="#7F3992"
+          />
+          <MyGamesRecents
+            numbers="01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25"
+            priceDate="30/11/2020 - (R$ 2,50)"
+            typeGame="Lotofácil"
+            typeColor="#7F3992"
+          />
+          <MyGamesRecents
+            numbers="01, 02,04,05,06,07,09,15,17,20,21,22,23,24,25"
+            priceDate="30/11/2020 - (R$ 2,50)"
+            typeGame="Lotofácil"
+            typeColor="#7F3992"
+          />
+        </div>
+        <div className="rightDiv">
+          <div className="DivContentNewBet">
+            <NavLink to="/new-bet" className="NewBet">
+              New Bet <BsArrowRight size={24} />
+            </NavLink>
+          </div>
+        </div>
+      </TwoViews>
+      <Footer content="Copyright 2021 Luby Software" />
     </>
-}
+  );
+};
 
 export default Home;
--- src/pages/NewBet.tsx
+++ src/pages/NewBet.tsx
@@ -1,9 +1,9 @@
-import ButtonGame from "@components/ButtonGame";
+import ButtonGame from "@components/Button/ButtonGame";
 import Cart from "@components/Cart";
-import CicleButton from "@components/CircleButton";
+import CicleButton from "@components/Button/CircleButton";
 import Footer from "@components/footer";
 import Header from "@components/Header";
-import RectangleButton from "@components/RectangleButton";
+import RectangleButton from "@components/Button/RectangleButton";
 import TwoViews from "@components/TwoViews";
 import { headerBase, urlBase } from "@src/constants/api_constants";
 import useLogout from "@src/hooks/logout";
@@ -13,80 +13,116 @@ import { useEffect, useState } from "react";
 import { BsArrowRight } from "react-icons/bs";
 import { BsCart3 } from "react-icons/bs";
 
+const NewBet = () => {
+  const logoutHandler = useLogout();
+  const [games, setGames] = useState<Game[]>([]);
+  const [chooseGame, setChooseGame] = useState(0);
+  const [ball, setBall] = useState(0);
 
-const NewBet=()=>{
-    const logoutHandler = useLogout()
-    const [games,setGames]= useState<Game[]>([])
-    const [chooseGame,setChooseGame]=useState(0)
-    const [ball,setBall]=useState(0)
-        
-    const serveData= async ()=>
-    {
-        const response = await axios.get(urlBase+'cart_games',{headers:headerBase}) 
-        const data= await response.data
-        setGames(data.types)
-        setChooseGame(data.types[0].id)
-        setBall(data.types[0].range)
+  const serveData = async () => {
+    const response = await axios.get(urlBase + "cart_games", {
+      headers: headerBase,
+    });
+    const data = await response.data;
+    setGames(data.types);
+    setChooseGame(data.types[0].id);
+    setBall(data.types[0].range);
+  };
+
+  const chooseGameHandler = (choose: number, index: number) => {
+    setChooseGame(choose);
+    console.log(ball);
+    setBall(games[index].range);
+  };
+
+  const generateBalls = () => {
+    const vet: JSX.Element[] = [];
+    for (let i = 1; i <= ball; i++) {
+      vet.push(<CicleButton key={i} number={i <= 9 ? "0" + i : "" + i} />);
     }
+    return vet;
+  };
 
-    const chooseGameHandler=(choose:number,index:number)=>
-    {
-        setChooseGame(choose)
-        console.log(ball)
-        setBall(games[index].range)
-    }
+  useEffect(() => {
+    serveData();
+  }, []);
 
-    const generateBalls=()=>{
-        const vet:JSX.Element[]=[]
-        for(let i=1;i<=ball;i++)
-        {
-            vet.push(<CicleButton key={i} number={i<=9 ? '0'+i : ''+i}/>)
-        }     
-        return vet;
-    }
+  return (
+    <>
+      <Header>
+        <div className="TGLHome">
+          TGL
+          <div />
+        </div>
+        <div className="TwoElements">
+          <span>
+            <span>Account</span>
+          </span>
+          <button onClick={logoutHandler}>
+            Log out &nbsp; <BsArrowRight size={24} />
+          </button>
+        </div>
+      </Header>
+      <TwoViews header={true}>
+        <div className="leftDiv">
+          <div className="region-game">
+            <div className="titles">
+              <span className="title-bold">New bet</span>{" "}
+              <span className="title">for mega-sena</span>
+            </div>
+            <div className="subtitle">Choose a game</div>
+            <div className="game-change">
+              {games.map((game: Game, index) => {
+                if (game.id === chooseGame) {
+                  return (
+                    <ButtonGame
+                      index={index}
+                      id={game.id}
+                      chooseHandler={chooseGameHandler}
+                      key={game.id}
+                      text={game.type}
+                      backGround={game.color}
+                      colorText="white"
+                    />
+                  );
+                }
+                return (
+                  <ButtonGame
+                    index={index}
+                    id={game.id}
+                    chooseHandler={chooseGameHandler}
+                    key={game.id}
+                    text={game.type}
+                    backGround="white"
+                    colorText={game.color}
+                  />
+                );
+              })}
+            </div>
+            <div className="subtitle">Fill your bet</div>
+            <div>
+              {" "}
+              Mark as many numbers as you want up to a maximum of 50. Win by
+              hitting 15, 16, 17, 18, 19, 20 or none of the 20 numbers drawn.
+            </div>
+            <div className="number-chooses">{generateBalls()}</div>
+            <div>
+              <RectangleButton>Complete game</RectangleButton>
+              <RectangleButton>Clear game</RectangleButton>
+              <RectangleButton>
+                <BsCart3 size={16} color="red" />
+                <span className="separate">Add to cart</span>
+              </RectangleButton>
+            </div>
+          </div>
+        </div>
+        <div className="rightDiv">
+          <Cart />
+        </div>
+      </TwoViews>
+      <Footer content="Copyright 2021 Luby Software" />
+    </>
+  );
+};
 
-    useEffect(()=>{
-     serveData()
-    },[])
-
-    return <>
-            <Header>
-                <div className="TGLHome">TGL<div/></div>
-                <div className="TwoElements"><span><span>Account</span></span><button onClick={logoutHandler}>Log out &nbsp; <BsArrowRight size={24}/></button></div>
-            </Header>
-            <TwoViews header={true}>
-                <div className="leftDiv">
-                    <div className="region-game">
-                            <div className="titles"><span className="title-bold">New bet</span> <span className="title">for mega-sena</span></div>
-                            <div className="subtitle">Choose a game</div>
-                            <div className="game-change">
-                                {games.map((game:Game,index)=>{                                  
-                                    if(game.id===chooseGame)
-                                    {
-                                        return <ButtonGame index={index} id={game.id} chooseHandler={chooseGameHandler} key={game.id} text={game.type} backGround={game.color} colorText="white"/>
-                                    }                                  
-                                   return  <ButtonGame  index={index} id={game.id} chooseHandler={chooseGameHandler} key={game.id} text={game.type} backGround="white" colorText={game.color}/>
-                                })}              
-                            </div>
-                            <div className="subtitle">Fill your bet</div>
-                            <div> Mark as many numbers as you want up to a maximum of 50. Win by hitting 15, 16, 17, 18, 19, 20 or none of the 20 numbers drawn.</div>
-                            <div className="number-chooses">
-                            {generateBalls()}                         
-                            </div>
-                            <div>
-                                <RectangleButton>Complete game</RectangleButton>
-                                <RectangleButton>Clear game</RectangleButton>
-                                <RectangleButton><BsCart3 size={16} color="red"/><span className="separate">Add to cart</span></RectangleButton>
-                            </div>
-                    </div>
-                  
-                </div>
-                <div className="rightDiv" >
-                    <Cart/>
-                </div>
-            </TwoViews>
-            <Footer content="Copyright 2021 Luby Software"/> 
-          </> 
-} 
-
-export default NewBet
+export default NewBet;
