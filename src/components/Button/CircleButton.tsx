795485058c746dd8f2dc8d48db2844c786128a3e
alanbruno1994 <bruno47rios@outlook.com>
2021-12-20 09:34:59 -0300

    feat: add styled and logic for CircleButton

--- src/components/Button/CircleButton.tsx
+++ src/components/Button/CircleButton.tsx
@@ -1,23 +1,53 @@
-import styled from "styled-components"
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-const Button=styled.button`
-    width: 65px;
-    height: 65px;
-    border-radius: 50%;
-    border:0px;
-    background: #ADC0C4;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    font-size: 20px;
-    font-weight: bold;
-    margin: 8px 5px;
-`
+const Button = styled.button<{ background?: string; hover: string }>`
+  width: 65px;
+  height: 65px;
+  border-radius: 50%;
+  border: 0px;
+  background: ${(props) => (props.background ? props.background : "#ADC0C4")};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  margin: 8px 5px;
 
-const CicleButton:React.FC<{number:string}>=(props)=>{
-    return <Button><span>{props.number}</span></Button>    
-}
+  &:hover {
+    background-color: ${(props) => props.hover};
+  }
 
+  @media (max-width: 705) {
+    width: 50px;
+    height: 50px;
+  }
+`;
 
-export default CicleButton
+const CicleButton: React.FC<{
+  number: string;
+  index: number;
+  action: (value: number) => void;
+  ballChoose: number[];
+  setBallChoose: (value: number[]) => void;
+}> = (props) => {
+  const [marked, setMaked] = useState<boolean>(false);
+
+  useEffect(() => {
+    const value = props.ballChoose.find((value) => value === props.index);
+    setMaked(!!value);
+  }, [props]);
+
+  return (
+    <Button
+      background={marked ? "#01AC66" : "#ADC0C4"}
+      hover={marked ? "#02d981" : "#b8cbcf"}
+      onClick={(_) => props.action(props.index)}
+    >
+      <span>{props.number}</span>
+    </Button>
+  );
+};
+
+export default CicleButton;
