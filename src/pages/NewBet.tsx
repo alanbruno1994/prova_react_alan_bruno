1cf20f1d6436e0beca16820704d489c6ff44adc0
alanbruno1994 <bruno47rios@outlook.com>
2021-12-21 09:07:50 -0300

    feat: add CardAnimation and modify code

--- src/pages/NewBet.tsx
+++ src/pages/NewBet.tsx
@@ -1,93 +1,35 @@
 import Cart from "@components/Cart";
 import Footer from "@components/footer";
 import TwoViews from "@components/TwoViews";
-import { headerBase, urlBase } from "@src/constants/api_constants";
-import Game from "@src/types/game.type";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import ActionCart from "@components/ActionsCart";
-import NumbersChoose from "@components/NumbersChoose";
 import Header from "@components/Header/Header";
-import ButtonsChose from "@components/ButtonsChose";
-import Error from "@components/UI/Error";
 import useFailure from "@src/hooks/failure";
-import mensagesFailure from "@src/common/messages_failure";
 import ErrorPortal from "@src/portals/ErrorPortal";
+import useGames from "@src/hooks/games";
+import RegionGame from "@components/Region/RegionGame";
+import CardAnimation from "@src/animation/CardMsgAnimation";
+import { useSelector } from "react-redux";
 
 const NewBet = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [chooseGame, setChooseGame] = useState({ id: 0, index: 0 });
-  const [ballChoose, setBallChoose] = useState<number[]>([]);
   const { failure, openFailure, closeFailure } = useFailure();
-  const [minCart, setMinCart] = useState(0);
-
-  const serveData = async () => {
-    try {
-      const response = await axios.get(urlBase + "cart_games", {
-        headers: headerBase,
-      });
-      const data = await response.data;
-      setGames(data.types);
-      setChooseGame({ id: data.types[0].id, index: 0 });
-      setMinCart(data.min_cart_value);
-    } catch (failure: any) {
-      mensagesFailure(failure, openFailure);
-    }
-  };
-
-  const clearGameHandler = () => {
-    setBallChoose([]);
-  };
-
-  const setChooseGameHandler = (value: { id: number; index: number }) => {
-    setChooseGame(value);
-    clearGameHandler();
-  };
-
-  useEffect(() => {
-    serveData();
-  }, []);
+  const { games, minCart } = useGames(openFailure);
+  const cart = useSelector((state: any) => state.cart);
 
   return (
     <>
       <ErrorPortal>
-        {failure.enable && (
-          <Error menssage={failure.message} close={closeFailure} />
-        )}
+        <CardAnimation
+          enable={failure.enable}
+          menssage={failure.message}
+          closeEnable={closeFailure}
+        ></CardAnimation>
       </ErrorPortal>
       <Header />
       <TwoViews header={true}>
-        <div className="leftDiv">
-          <div className="region-game">
-            <div className="titles">
-              <span className="title-bold">New bet</span>{" "}
-              <span className="title">for mega-sena</span>
-            </div>
-            <div className="subtitle">Choose a game</div>
-            <ButtonsChose
-              chooseGame={chooseGame}
-              setChooseGame={setChooseGameHandler}
-              games={games}
-            />
-            <div className="subtitle">Fill your bet</div>
-            <div>{games[chooseGame.index]?.description}</div>
-            <NumbersChoose
-              setBallChoose={setBallChoose}
-              chooseGame={chooseGame}
-              games={games}
-              ballChoose={ballChoose}
-            />
-            <ActionCart
-              setBallChoose={setBallChoose}
-              chooseGame={chooseGame}
-              clearGameHandler={clearGameHandler}
-              games={games}
-              ballChoose={ballChoose}
-            />
-          </div>
+        <div className={cart.items.length > 0 ? "leftDiv" : "full-width"}>
+          <RegionGame games={games} />
         </div>
-        <div className="rightDiv">
-          <Cart minCart={minCart} />
+        <div className={cart.items.length > 0 ? "rightDiv" : ""}>
+          {cart.items.length > 0 && <Cart minCart={minCart} />}
         </div>
       </TwoViews>
       <Footer content="Copyright 2021 Luby Software" />
