7310835d76f29b4a8ebabb81b4f1889349ab8547
alanbruno1994 <bruno47rios@outlook.com>
2021-12-21 13:35:06 -0300

    feat: insert animation of loading contents in MyGamesRecents

--- src/components/MyGamesRecents.tsx
+++ src/components/MyGamesRecents.tsx
@@ -1,3 +1,6 @@
+import animationTiming from "@src/constants/timing_animation";
+import { useEffect, useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import Content from "./SyledComponents/MyGamesRecentsStyled";
 
 const MyGamesRecents: React.FC<{
@@ -6,14 +9,35 @@ const MyGamesRecents: React.FC<{
   typeGame: string;
   typeColor: string;
 }> = (props) => {
+  const [enable, setEnable] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setEnable(true), 200);
+  }, []);
+
   return (
     <Content typeColor={props.typeColor}>
-      <div className="bar"></div>
-      <div className="infos-recent-games">
-        <span className="numbers">{props.numbers}</span>
-        <span className="price-date">{props.priceDate}</span>
-        <span className="type-game">{props.typeGame}</span>
-      </div>
+      <CSSTransition
+        mountOnEnter //isso aqui e usado para que componente aparece na dom na hora de exibir
+        unmountOnExit // Isso aqui e para tiar da dom na hora de esconder
+        in={enable} //Aqui serve para controlar quando esconde e quando mostrar
+        timeout={animationTiming}
+        classNames={{
+          enter: "",
+          enterActive: "init",
+          exit: "",
+          exitActive: "exit",
+        }}
+      >
+        <>
+          <div className="bar"></div>
+          <div className="infos-recent-games">
+            <span className="numbers">{props.numbers}</span>
+            <span className="price-date">{props.priceDate}</span>
+            <span className="type-game">{props.typeGame}</span>
+          </div>
+        </>
+      </CSSTransition>
     </Content>
   );
 };
